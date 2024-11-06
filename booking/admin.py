c0b38c0fedd2cac7305d6ca94d3dc8b89db76882
R.N <148905439+RNXVIII@users.noreply.github.com>
2024-11-06 23:45:44 +0000

    added summernote

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+    list_display = ('id', 'date', 'table_number', 'user', 'booked')
+    search_fields = ['user__username']
+    list_filter = ('booked',)
