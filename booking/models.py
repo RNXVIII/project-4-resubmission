99948976fd9754d02317bb351f6ef47f463bfb41
R.N <148905439+RNXVIII@users.noreply.github.com>
2024-11-15 02:34:29 +0000

    cleaning up css and other files

--- booking/models.py
+++ booking/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-STATUS = ((0, "Draft"), (1, "Published"))
-
-
 
 class Booking(models.Model):
     id = models.AutoField(primary_key=True)
